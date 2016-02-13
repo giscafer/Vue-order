320df597969430384d299dd393b443663f9d64e9
giscafer <giscafer@outlook.com>
2016-02-13 21:57:00 +0800

    for ci

--- src/server/test/proxy/user.test.js
+++ src/server/test/proxy/user.test.js
@@ -1,4 +1,6 @@
 var UserProxy=require('../../proxy/user.js');
 var should=require('chai').should();
 
-describe('test/proxy/user.test.js',function(){});
+describe('test/proxy/user.test.js',function(){
+	
+});
